3d5c8ce1be32d38829203e33778e06bdf878213c
DenisSuperStar <DabBr1994@yandex.ru>
2023-07-13 06:40:56 +0300

    Создан вызов дочернего конструктора в Lemonade

--- Items.Domain/Models/Categories/Drinks/ConcreteLemonade/Lemonade/Lemonade.cs
+++ Items.Domain/Models/Categories/Drinks/ConcreteLemonade/Lemonade/Lemonade.cs
@@ -1,51 +1,12 @@
 ﻿using Items.Domain.Models.Categories.Common;
-using Items.Domain.Models.Categories.Drinks.ConcreteWater.Water;
+using Items.Domain.Models.Categories.Drinks.ConcreateWater.Water;
 
 namespace Items.Domain.Models.Categories.Drinks.ConcreteLemonade.Lemonade
 {
     public class Lemonade : Water
     {
-        public Lemonade(Guid personId, Guid itemId, string itemName, int price, string imagePath, string maker, int minTemp, int maxTemp, float protein, float fat, float sugar, float energy, int countInPackage, int createdDate, List<Taste> tastes, List<Capacity> capacities, bool isChilled, bool isSparkling, string purpose)
+        public Lemonade(Guid personId, Guid itemId, string itemName, int price, string imagePath, string maker, int minTemp, int maxTemp, float protein, float fat, float sugar, float energy, int countInPackage, int createdDate, List<Taste> tastes, List<Capacity> capacities, bool isChilled, bool isSparkling, string purpose) : base(personId, itemId, itemName, price, imagePath, maker, minTemp, maxTemp, protein, fat, sugar, energy, countInPackage, createdDate, tastes, capacities, isChilled, isSparkling, purpose)
         {
-            PersonId = personId;
-            ItemId = itemId;
-            ItemName = itemName;
-            Price = price;
-            ImagePath = imagePath;
-            Maker = maker;
-            MinTemp = minTemp;
-            MaxTemp = maxTemp;
-            Protein = protein;
-            Fat = fat;
-            Sugar = sugar;
-            Energy = energy;
-            CountInPackage = countInPackage;
-            CreatedDate = createdDate;
-            Tastes = tastes;
-            Capacities = capacities;
-            IsChilled = isChilled;
-            IsSparkling = isSparkling;
-            Purpose = purpose;
         }
-
-        public Guid PersonId { get; }
-        public Guid ItemId { get; }
-        public string ItemName { get; }
-        public int Price { get; }
-        public string ImagePath { get; }
-        public string Maker { get; }
-        public int MinTemp { get; }
-        public int MaxTemp { get; }
-        public float Protein { get; }
-        public float Fat { get; }
-        public float Sugar { get; }
-        public float Energy { get; }
-        public int CountInPackage { get; }
-        public int CreatedDate { get; }
-        public List<Taste> Tastes { get; }
-        public List<Capacity> Capacities { get; }
-        public bool IsChilled { get; }
-        public bool IsSparkling { get; }
-        public string Purpose { get; }
     }
 }
