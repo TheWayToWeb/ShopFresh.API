6c00c8bd720ccf5b96e2913cadcd31800828b5a1
DenisSuperStar <DabBr1994@yandex.ru>
2023-08-16 22:11:45 +0300

    Добавлен класс Отзыв

--- Items.Domain/Review/Review.cs
+++ Items.Domain/Review/Review.cs
@@ -1,6 +1,15 @@
-﻿namespace Items.Domain.Review
+﻿using Items.Domain.Components.Entities.Categories.Common;
+
+namespace Items.Domain.Review
 {
     public class Review
     {
+        public Guid ReviewId { get; private set; }
+        public Guid PersonId { get; private set; }
+        public Guid ItemId { get; private set; }
+        public int Rating { get; private set; }
+        public string? Description { get; private set; }
+        // public Person Person { get; set; }
+        public Item? Item { get; set; }
     }
 }
