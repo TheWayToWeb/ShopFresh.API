83265e05fe51d6347480a3742ee55e32dd00e886
DenisSuperStar <DabBr1994@yandex.ru>
2023-08-30 22:09:25 +0300

    Добавлено возрастное ограничение к употреблению AgeLimit во FruitDrink Написана View Model of fruit drink vm Добавлен View Model of fruit drink detail vm

---
+++ Items.Domain/ViewModel/EntitiesVm/FruitDrinks/FruitDrinkDetailVm.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Items.Domain.Components.Entities.FruitDrink;
+using Items.Domain.DTOs.Requests.Common;
+using Items.Domain.ViewModel.EntitiesVm.Common;
+
+namespace Items.Domain.ViewModel.EntitiesVm.FruitDrinks
+{
+    public class FruitDrinkDetailVm : ItemVmBase, IMapWith<FruitDrink>
+    {
+        public void Mapping(Profile profile)
+        {
+        }
+    }
+}
---
+++ Items.Domain/ViewModel/EntitiesVm/FruitDrinks/FruitDrinkVm.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Items.Domain.Components.Entities.Common;
+using Items.Domain.Components.Entities.FruitDrink;
+using Items.Domain.DTOs.Requests.Common;
+
+namespace Items.Domain.ViewModel.EntitiesVm.FruitDrinks
+{
+    public class FruitDrinkVm : IMapWith<FruitDrink>
+    {
+        public Guid Id { get; set; }
+        public string? BrandName { get; set; }
+        public string? ImagePath { get; set; }
+        public int Price { get; set; }
+        public int CountInPackage { get; set; }
+        public int AgeLimit { get; set; }
+        public List<SoldCapacity> Volume { get; set; } = new();
+
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<FruitDrink, FruitDrinkVm>()
+                .ForMember(map => map.Id,
+                    opt => opt.MapFrom(map => map.Id))
+                .ForMember(map => map.BrandName,
+                    opt => opt.MapFrom(map => map.Brand!.BrandName))
+                .ForMember(map => map.ImagePath,
+                    opt => opt.MapFrom(map => map.ImagePath))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price))
+                .ForMember(map => map.CountInPackage,
+                    opt => opt.MapFrom(map => map.CountInPackage))
+                .ForMember(map => map.AgeLimit,
+                    opt => opt.MapFrom(map => map.AgeLimit))
+                .ForMember(map => map.Volume,
+                    opt => opt.MapFrom(map => map.Volume));
+        }
+    }
+}
