b87e28620f73b1362f0d655ac1ce7a9c2659706e
DenisSuperStar <DabBr1994@yandex.ru>
2023-08-30 22:28:18 +0300

    Написана View Model для fruit drink детального представления

--- Items.Domain/ViewModel/EntitiesVm/FruitDrinks/FruitDrinkDetailVm.cs
+++ Items.Domain/ViewModel/EntitiesVm/FruitDrinks/FruitDrinkDetailVm.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Items.Domain.Components.Entities.Common;
 using Items.Domain.Components.Entities.FruitDrink;
 using Items.Domain.DTOs.Requests.Common;
 using Items.Domain.ViewModel.EntitiesVm.Common;
@@ -7,8 +8,54 @@ namespace Items.Domain.ViewModel.EntitiesVm.FruitDrinks
 {
     public class FruitDrinkDetailVm : ItemVmBase, IMapWith<FruitDrink>
     {
+        public List<ProductTaste> Tastes { get; set; } = new();
+        public List<SoldCapacity> Volume { get; set; } = new();
+        public int AgeLimit { get; set; }
+        public bool IsCold { get; set; }
+
         public void Mapping(Profile profile)
         {
+            profile.CreateMap<FruitDrink, FruitDrinkDetailVm>()
+                .ForMember(map => map.Id,
+                    opt => opt.MapFrom(map => map.Id))
+                .ForMember(map => map.BrandName,
+                    opt => opt.MapFrom(map => map.Brand!.BrandName))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price))
+                .ForMember(map => map.ImagePath,
+                    opt => opt.MapFrom(map => map.ImagePath))
+                .ForMember(map => map.Maker,
+                    opt => opt.MapFrom(map => map.Maker))
+                .ForMember(map => map.CountInPackage,
+                    opt => opt.MapFrom(map => map.CountInPackage))
+                .ForMember(map => map.Weight,
+                    opt => opt.MapFrom(map => map.Weight))
+                .ForMember(map => map.Description,
+                    opt => opt.MapFrom(map => map.Description))
+                .ForMember(map => map.IsFarmer,
+                    opt => opt.MapFrom(map => map.IsFarmer))
+                .ForMember(map => map.Protein,
+                    opt => opt.MapFrom(map => map.Protein))
+                .ForMember(map => map.Fat,
+                    opt => opt.MapFrom(map => map.Fat))
+                .ForMember(map => map.Sugar,
+                    opt => opt.MapFrom(map => map.Sugar))
+                .ForMember(map => map.Energy,
+                    opt => opt.MapFrom(map => map.Energy))
+                .ForMember(map => map.CreatedDate,
+                    opt => opt.MapFrom(map => map.CreatedDate))
+                .ForMember(map => map.MinTemp,
+                    opt => opt.MapFrom(map => map.MinTemp))
+                .ForMember(map => map.MaxTemp,
+                    opt => opt.MapFrom(map => map.MaxTemp))
+                .ForMember(map => map.Tastes,
+                    opt => opt.MapFrom(map => map.Tastes))
+                .ForMember(map => map.Volume,
+                    opt => opt.MapFrom(map => map.Volume))
+                .ForMember(map => map.AgeLimit,
+                    opt => opt.MapFrom(map => map.AgeLimit))
+                .ForMember(map => map.IsCold,
+                    opt => opt.MapFrom(map => map.IsCold));
         }
     }
 }
