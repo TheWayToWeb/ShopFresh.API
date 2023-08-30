b2b3ee0dd3ca591f64ff6553e6a6ec2ba15a632a
DenisSuperStar <DabBr1994@yandex.ru>
2023-08-30 11:37:49 +0300

    Доработана ViewModel для CoffeeDetailVm

--- Items.Domain/ViewModel/Entities/Creams/CreamDetailVm.cs
+++ Items.Domain/ViewModel/Entities/Creams/CreamDetailVm.cs
@@ -30,9 +30,9 @@ namespace Items.Domain.ViewModel.Entities.Creams
 
         public void Mapping(Profile profile)
         {
-            profile.CreateMap<CreamVm, CreamDetailVm>()
+            profile.CreateMap<Cream, CreamDetailVm>()
                 .ForMember(map => map.Id,
-                    opt => opt.MapFrom(map => map))
+                    opt => opt.MapFrom(map => map.Id))
                 .ForMember(map => map.BrandName,
                     opt => opt.MapFrom(map => map.Brand!.BrandName))
                 .ForMember(map => map.Price,
@@ -43,7 +43,33 @@ namespace Items.Domain.ViewModel.Entities.Creams
                     opt => opt.MapFrom(map => map.Maker))
                 .ForMember(map => map.CountInPackage,
                     opt => opt.MapFrom(map => map.CountInPackage))
-                .ForMember
+                .ForMember(map => map.Description,
+                    opt => opt.MapFrom(map => map.Description))
+                .ForMember(map => map.IsFarmer,
+                    opt => opt.MapFrom(map => map.IsFarmer))
+                .ForMember(map => map.Protein,
+                    opt => opt.MapFrom(map => map.Protein))
+                .ForMember(map => map.Fat,
+                    opt => opt.MapFrom(map => map.Fat))
+                .ForMember(map => map.Sugar,
+                    opt => opt.MapFrom(map => map.Sugar))
+                .ForMember(map => map.Energy,
+                    opt => opt.MapFrom(map => map.Energy))
+                .ForMember(map => map.CreatedDate,
+                    opt => opt.MapFrom(map => map.CreatedDate))
+                .ForMember(map => map.MinTemp,
+                    opt => opt.MapFrom(map => map.MinTemp))
+                .ForMember(map => map.MaxTemp,
+                    opt => opt.MapFrom(map => map.MaxTemp))
+                .ForMember(map => map.Taste,
+                    opt => opt.MapFrom(map => map.Taste))
+                .ForMember(map => map.CreamFats,
+                    opt => opt.MapFrom(map => map.CreamFats))
+                .ForMember(map => map.MilkProcessing,
+                    opt => opt.MapFrom(map => map.MilkProcessing))
+                .ForMember(map => map.PrimaryProduct,
+                    opt => opt.MapFrom(map => map.PrimaryProduct));
+
 
         }
     }
