233854d6212c8197915e9e4f44ba0c7a397b829b
DenisSuperStar <DabBr1994@yandex.ru>
2023-08-30 21:06:19 +0300

    Рефакторинг класса View Model of cheesy product detail vm

--- Items.Domain/ViewModel/Entities/CheesyProducts/CheesyProductDetailVm.cs
+++ Items.Domain/ViewModel/Entities/CheesyProducts/CheesyProductDetailVm.cs
@@ -2,27 +2,12 @@
 using Items.Domain.Components.Entities.CheesyProducts;
 using Items.Domain.Components.Entities.Common;
 using Items.Domain.DTOs.Requests.Common;
+using Items.Domain.ViewModel.Entities.Common;
 
 namespace Items.Domain.ViewModel.Entities.CheesyProducts
 {
-    public class CheesyProductDetailVm : IMapWith<CheesyProduct>
+    public class CheesyProductDetailVm : ItemVmBase, IMapWith<CheesyProduct>
     {
-        public Guid Id { get; set; }
-        public string? BrandName { get; set; }
-        public string? ImagePath { get; set; }
-        public int Price { get; set; }
-        public string? Maker { get; set; }
-        public int CountInPackage { get; set; }
-        public float Weight { get; set; }
-        public string? Description { get; set; }
-        public bool IsFarmer { get; set; }
-        public float Protein { get; set; }
-        public float Fat { get; set; }
-        public float Sugar { get; set; }
-        public float Energy { get; set; }
-        public int CreatedDate { get; set; }
-        public int MinTemp { get; set; }
-        public int MaxTemp { get; set; }
         public List<ProductTaste> Tastes { get; set; } = new();
         public string? FatContent { get; set; }
         public string? KindRelease { get; set; }
