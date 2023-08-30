7e383ec7373854d0948cb9d438c69cd729e82066
DenisSuperStar <DabBr1994@yandex.ru>
2023-08-30 21:04:18 +0300

    Исправлена опечатка в именовании списка в Butter.cs Рефакторинг класса View Model of butter detail vm

--- Items.Domain/Components/Entities/Butter/Butter.cs
+++ Items.Domain/Components/Entities/Butter/Butter.cs
@@ -4,7 +4,7 @@ namespace Items.Domain.Components.Entities.Butter
 {
     public class Butter : ItemFood
     {
-        public List<ProductTaste> Taste { get; private set; } = new();
+        public List<ProductTaste> Tastes { get; private set; } = new();
         public List<FatContentProduct> FatContents { get; private set; } = new();
         public bool IsVegan { get; private set; }
     }
--- Items.Domain/ViewModel/Entities/Butters/ButterDetailVm.cs
+++ Items.Domain/ViewModel/Entities/Butters/ButterDetailVm.cs
@@ -14,6 +14,45 @@ namespace Items.Domain.ViewModel.Entities.Butters
 
         public void Mapping(Profile profile)
         {
+            profile.CreateMap<Butter, ButterDetailVm>()
+                .ForMember(map => map.Id,
+                    opt => opt.MapFrom(map => map.Id))
+                .ForMember(map => map.BrandName,
+                    opt => opt.MapFrom(map => map.Brand!.BrandName))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price))
+                .ForMember(map => map.ImagePath,
+                    opt => opt.MapFrom(map => map.ImagePath))
+                .ForMember(map => map.Maker,
+                    opt => opt.MapFrom(map => map.Maker))
+                .ForMember(map => map.CountInPackage,
+                    opt => opt.MapFrom(map => map.CountInPackage))
+                .ForMember(map => map.Weight,
+                    opt => opt.MapFrom(map => map.Weight))
+                .ForMember(map => map.Description,
+                    opt => opt.MapFrom(map => map.Description))
+                .ForMember(map => map.IsFarmer,
+                    opt => opt.MapFrom(map => map.IsFarmer))
+                .ForMember(map => map.Protein,
+                    opt => opt.MapFrom(map => map.Protein))
+                .ForMember(map => map.Fat,
+                    opt => opt.MapFrom(map => map.Fat))
+                .ForMember(map => map.Sugar,
+                    opt => opt.MapFrom(map => map.Sugar))
+                .ForMember(map => map.Energy,
+                    opt => opt.MapFrom(map => map.Energy))
+                .ForMember(map => map.CreatedDate,
+                    opt => opt.MapFrom(map => map.CreatedDate))
+                .ForMember(map => map.MinTemp,
+                    opt => opt.MapFrom(map => map.MinTemp))
+                .ForMember(map => map.MaxTemp,
+                    opt => opt.MapFrom(map => map.MaxTemp))
+                .ForMember(map => map.Tastes,
+                    opt => opt.MapFrom(map => map.Tastes))
+                .ForMember(map => map.FatContents,
+                    opt => opt.MapFrom(map => map.FatContents))
+                .ForMember(map => map.IsVegan,
+                    opt => opt.MapFrom(map => map.IsVegan));
         }
     }
 }
