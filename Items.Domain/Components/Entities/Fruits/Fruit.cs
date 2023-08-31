a4bd86217219ac4b62337d3e7a0d445e7e952ada
DenisSuperStar <DabBr1994@yandex.ru>
2023-08-31 21:24:50 +0300

    Добавлено свойство by weight в класс Fruit Написан класс View Model для товарной категори фрукты

--- Items.Domain/Components/Entities/Fruits/Fruit.cs
+++ Items.Domain/Components/Entities/Fruits/Fruit.cs
@@ -9,6 +9,7 @@ namespace Items.Domain.Components.Entities.Fruits
         public string? FormRelease { get; private set; }
         public string? CoolingMode { get; private set; }
         public string? SkinColor { get; private set; }
+        public bool ByWeight { get; set; }
         public List<SalesLine> SalesWeights { get; private set; } = new();
     }
 }
