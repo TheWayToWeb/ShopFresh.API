d15f01b5a6aaa3b97be97ee41e115412cb183b4a
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-01 05:09:37 +0300

    Добавлено свойство ByWeight в класс Vegetable Написан класс ViewModel для каталога овощи Написан класс View Model для детальной страницы овощи

--- Items.Domain/Components/Entities/Vegetables/Vegetable.cs
+++ Items.Domain/Components/Entities/Vegetables/Vegetable.cs
@@ -9,6 +9,7 @@ namespace Items.Domain.Components.Entities.Vegetables
         public string? FormRelease { get; private set; }
         public string? CoolingMode { get; private set; }
         public string? SkinColor { get; private set; }
+        public bool ByWeight { get; private set; }
         public List<SalesRange> SalesWeights { get; private set; } = new();
     }
 }
