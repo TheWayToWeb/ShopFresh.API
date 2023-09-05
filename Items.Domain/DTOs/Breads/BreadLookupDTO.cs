6d125b8bd0ae2932aede6ae9291349c5aa4be2f5
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-05 23:51:57 +0300

    Написан класс DTO для каталога хлеб

---
+++ Items.Domain/DTOs/Breads/BreadLookupDTO.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Items.Domain.Components.Entities.Bread;
+using Items.Domain.DTOs.Common;
+
+namespace Items.Domain.DTOs.Breads
+{
+    public class BreadLookupDTO : IMapWith<Bread>
+    {
+        public Guid Id { get; set; }
+        public string? BrandName { get; set; }
+        public string? ImagePath { get; set; }
+        public int Price { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Bread, BreadLookupDTO>()
+                .ForMember(map => map.Id,
+                    opt => opt.MapFrom(map => map.Id))
+                .ForMember(map => map.BrandName,
+                    opt => opt.MapFrom(map => map.Brand!.BrandName))
+                .ForMember(map => map.ImagePath,
+                    opt => opt.MapFrom(map => map.ImagePath))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price));
+        }
+    }
+}
