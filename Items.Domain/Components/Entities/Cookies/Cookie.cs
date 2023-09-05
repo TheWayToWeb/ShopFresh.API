bcbe1db61a07dc8a36992c9b8567fd8a5b529b44
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-05 23:17:50 +0300

    Написан класс структуры таблицы печенье

---
+++ Items.Domain/Components/Entities/Cookies/Cookie.cs
@@ -0,0 +1,9 @@
+﻿using Items.Domain.Components.Entities.Common;
+
+namespace Items.Domain.Components.Entities.Cookies
+{
+    public class Cookie : ItemFood
+    {
+        public string? GlazeType { get; set; }
+    }
+}
