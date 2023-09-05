16e43ca0a87c67314843d6c8f11ee31ffc04c3fd
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-05 23:51:27 +0300

    Написан класс модели таблицы хлеб

--- Items.Domain/Components/Entities/Bread/Bread.cs
+++ Items.Domain/Components/Entities/Bread/Bread.cs
@@ -1,12 +1,8 @@
-﻿using Items.Domain.Components.Entities.Categories.Common;
+﻿using Items.Domain.Components.Entities.Common;
 
 namespace Items.Domain.Components.Entities.Bread
 {
-    public abstract class Bread : Eat
+    public class Bread : ItemFood
     {
-        public string? KindOfBread { get; set; }
-        public string? Feature { get; set; }
-        public string? Addition { get; set; }
-        public string? TypeOfFlour { get; set; }
     }
 }
