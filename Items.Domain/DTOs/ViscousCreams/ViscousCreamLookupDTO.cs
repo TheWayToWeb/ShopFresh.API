39c6aa797e18fb5805f14a4f5d3546493943b094
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-05 22:05:14 +0300

    Написан класс DTO для каталога сметаны, варенца, простокваши

---
+++ Items.Domain/DTOs/ViscousCreams/ViscousCreamLookupDTO.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Items.Domain.Components.Entities.ViscousCream;
+using Items.Domain.DTOs.Common;
+
+namespace Items.Domain.DTOs.ViscousCreams
+{
+    public class ViscousCreamLookupDTO : IMapWith<ViscousCream>
+    {
+        public Guid Id { get; set; }
+        public string? BrandName { get; set; }
+        public string? ImagePath { get; set; }
+        public int Price { get; set; }
+        public float Weight { get; set; }
+
+        public void Mappin(Profile profile)
+        {
+            profile.CreateMap<ViscousCream, ViscousCreamLookupDTO>()
+                .ForMember(map => map.Id,
+                    opt => opt.MapFrom(map => map.Id))
+                .ForMember(map => map.BrandName,
+                    opt => opt.MapFrom(map => map.Brand!.BrandName))
+                .ForMember(map => map.ImagePath,
+                    opt => opt.MapFrom(map => map.ImagePath))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price))
+                .ForMember(map => map.Weight,
+                    opt => opt.MapFrom(map => map.Weight));
+        }
+    }
+}
