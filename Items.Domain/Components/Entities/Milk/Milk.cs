2ca7dc9637890332547a29a874b0a893f803e2be
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-05 23:42:11 +0300

    Изменено поле Volume в классе Milk

--- Items.Domain/Components/Entities/Milk/Milk.cs
+++ Items.Domain/Components/Entities/Milk/Milk.cs
@@ -6,7 +6,7 @@ namespace Items.Domain.Components.Entities.Milk
     {
         public List<ProductTaste> Tastes { get; private set; } = new();
         public List<FatContentProduct> FatContents { get; private set; } = new();
-        public List<SoldCapacity> Volume { get; private set; } = new();
+        public float Volume { get; set; }
         public bool IsMilkSugarFree { get; private set; }
     }
 }
