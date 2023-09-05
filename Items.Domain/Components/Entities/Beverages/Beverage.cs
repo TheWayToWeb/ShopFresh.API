81c2a76b07117bb48b7ab7c41d992a8ae4c36663
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-05 20:42:59 +0300

    Написан класс DTO для напитков

---
+++ Items.Domain/Components/Entities/Beverages/Beverage.cs
@@ -0,0 +1,12 @@
+﻿using Items.Domain.Components.Entities.Common;
+
+namespace Items.Domain.Components.Entities.Water
+{
+    public class Beverage : ItemFood
+    {
+        public List<ProductTaste> Tates { get; private set; } = new();
+        public List<SoldCapacity> Volume { get; private set; } = new();
+        public string? KindRelease { get; private set; }
+        public bool IsCold { get; private set; }
+    }
+}
