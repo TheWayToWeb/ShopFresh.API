9610fe113b351b7c6139c4c0d61f542a2b7c88a5
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-05 23:19:05 +0300

    Написан класс струкутры таблицы тортов

---
+++ Items.Domain/Components/Entities/Cakes/Cake.cs
@@ -0,0 +1,9 @@
+﻿using Items.Domain.Components.Entities.Common;
+
+namespace Items.Domain.Components.Entities.Cakes
+{
+    public class FancyCake : ItemFood
+    {
+        public string? Taste { get; set; }
+    }
+}
