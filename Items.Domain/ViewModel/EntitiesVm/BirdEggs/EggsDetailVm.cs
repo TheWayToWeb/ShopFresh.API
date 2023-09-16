30c1d7f2ea64af7498845762ec3858fea0979da8
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-16 23:59:40 +0300

    Добавлен класс детальной View Model для отображение категории товара Яйца

--- Items.Domain/ViewModel/EntitiesVm/BirdEggs/EggsDetailVm.cs
+++ Items.Domain/ViewModel/EntitiesVm/BirdEggs/EggsDetailVm.cs
@@ -1,15 +1,28 @@
 ﻿using AutoMapper;
 using Items.Domain.Components.Entities.Eggs;
 using Items.Domain.DTOs.Common;
-using Items.Domain.ViewModel.EntitiesVm.Common;
 
 namespace Items.Domain.ViewModel.EntitiesVm.BirdEggs
 {
-    public class EggsDetailVm : ItemVmCommon, IMapWith<Egg>
+    public class EggsDetailVm : IMapWith<Egg>
     {
-        public string? Category { get; set; }
+        public Guid Id { get; set; }
+        public string? BrandName { get; set; }
+        public string? ImagePath { get; set; }
+        public int CountInPackage { get; set; }
         public string? Kind { get; set; }
-        public string? Size { get; set; }
+        public string? Category { get; set; }
+        public string? Description { get; set; }
+        public int Price { get; set; }
+        public float Protein { get; set; }
+        public float Fat { get; set; }
+        public float Sugar { get; set; }
+        public float Energy { get; set; }
+        public float MinTemp { get; set; }
+        public float MaxTemp { get; set; }
+        public bool IsFarmer { get; set; }
+        public int CreatedDate { get; set; }
+        public float Weight { get; set; }
 
         public void Mapping(Profile profile)
         {
@@ -18,20 +31,18 @@ namespace Items.Domain.ViewModel.EntitiesVm.BirdEggs
                     opt => opt.MapFrom(map => map.Id))
                 .ForMember(map => map.BrandName,
                     opt => opt.MapFrom(map => map.Brand!.BrandName))
-                .ForMember(map => map.Price,
-                    opt => opt.MapFrom(map => map.Price))
                 .ForMember(map => map.ImagePath,
                     opt => opt.MapFrom(map => map.ImagePath))
-                .ForMember(map => map.Maker,
-                    opt => opt.MapFrom(map => map.Maker))
                 .ForMember(map => map.CountInPackage,
                     opt => opt.MapFrom(map => map.CountInPackage))
-                .ForMember(map => map.Weight,
-                    opt => opt.MapFrom(map => map.Weight))
+                .ForMember(map => map.Kind,
+                    opt => opt.MapFrom(map => map.Kind))
+                .ForMember(map => map.Category,
+                    opt => opt.MapFrom(map => map.Category))
                 .ForMember(map => map.Description,
                     opt => opt.MapFrom(map => map.Description))
-                .ForMember(map => map.IsFarmer,
-                    opt => opt.MapFrom(map => map.IsFarmer))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price))
                 .ForMember(map => map.Protein,
                     opt => opt.MapFrom(map => map.Protein))
                 .ForMember(map => map.Fat,
@@ -40,18 +51,16 @@ namespace Items.Domain.ViewModel.EntitiesVm.BirdEggs
                     opt => opt.MapFrom(map => map.Sugar))
                 .ForMember(map => map.Energy,
                     opt => opt.MapFrom(map => map.Energy))
-                .ForMember(map => map.CreatedDate,
-                    opt => opt.MapFrom(map => map.CreatedDate))
                 .ForMember(map => map.MinTemp,
                     opt => opt.MapFrom(map => map.MinTemp))
                 .ForMember(map => map.MaxTemp,
                     opt => opt.MapFrom(map => map.MaxTemp))
-                .ForMember(map => map.Category,
-                    opt => opt.MapFrom(map => map.Category))
-                .ForMember(map => map.Kind,
-                    opt => opt.MapFrom(map => map.Kind))
-                .ForMember(map => map.Size,
-                    opt => opt.MapFrom(map => map.Size));
+                .ForMember(map => map.IsFarmer,
+                    opt => opt.MapFrom(map => map.IsFarmer))
+                .ForMember(map => map.CreatedDate,
+                    opt => opt.MapFrom(map => map.CreatedDate))
+                .ForMember(map => map.Weight,
+                    opt => opt.MapFrom(map => map.Weight));
         }
     }
 }
