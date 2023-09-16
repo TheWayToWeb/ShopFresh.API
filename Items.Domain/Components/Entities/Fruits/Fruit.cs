a220935988c137c22c67dad09da7e553d6305f8f
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-16 23:57:30 +0300

    Обновление модели Fruit

--- Items.Domain/Components/Entities/Fruits/Fruit.cs
+++ Items.Domain/Components/Entities/Fruits/Fruit.cs
@@ -4,11 +4,10 @@ namespace Items.Domain.Components.Entities.Fruits
 {
     public class Fruit : ItemFood
     {
-        public string? Grade { get; private set; }
+        public string? Kind { get; private set; }
         public string? FormRelease { get; private set; }
+        public string? Grade { get; private set; }
         public string? CoolingMode { get; private set; }
-        public string? SkinColor { get; private set; }
-        public bool ByWeight { get; set; }
-        public List<SalesRange> SalesWeights { get; private set; } = new();
+        public List<SalesRange> SaleWeights { get; set; } = new();
     }
 }
