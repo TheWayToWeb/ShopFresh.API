af212308ae09b2009bd2182b2f7148476d44c120
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-16 23:56:31 +0300

    Обновление модели Coffee

--- Items.Domain/Components/Entities/Coffee/Coffee.cs
+++ Items.Domain/Components/Entities/Coffee/Coffee.cs
@@ -4,12 +4,11 @@ namespace Items.Domain.Components.Entities.Coffee
 {
     public class Coffee : ItemFood
     {
+        public string? Type { get; set; }
         public List<ProductTaste> Tastes { get; private set; } = new();
-        public string? TypeCoffee { get; private set; }
-        public string? KindCoffee { get; private set; }
-        public string? ConsistencyRelease { get; private set; }
-        public string? Composition { get; private set; }
-        public string? IntencityRelease { get; private set; }
+        public string? Kind { get; set; }
+        public string? Technology { get; set; }
+        public string? Intencity { get; set; }
         public bool IsCoffeinFree { get; private set; }
     }
 }
