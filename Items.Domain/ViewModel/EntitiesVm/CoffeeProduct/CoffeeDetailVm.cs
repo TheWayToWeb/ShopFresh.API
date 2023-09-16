852c43372aff11cb2ceed0f8d26c3c2c323facc9
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-17 00:03:54 +0300

    Обновил View Model детального представления для категории товара Кофе

--- Items.Domain/ViewModel/EntitiesVm/CoffeeProduct/CoffeeDetailVm.cs
+++ Items.Domain/ViewModel/EntitiesVm/CoffeeProduct/CoffeeDetailVm.cs
@@ -2,19 +2,26 @@
 using Items.Domain.Components.Entities.Coffee;
 using Items.Domain.Components.Entities.Common;
 using Items.Domain.DTOs.Common;
-using Items.Domain.ViewModel.EntitiesVm.Common;
 
 namespace Items.Domain.ViewModel.EntitiesVm.CoffeeProduct
 {
-    public class CoffeeDetailVm : ItemVmCommon, IMapWith<Coffee>
+    public class CoffeeDetailVm : IMapWith<Coffee>
     {
+        public Guid Id { get; set; }
+        public string? BrandName { get; set; }
+        public string? ImagePath { get; set; }
+        public int Price { get; set; }
+        public string? Technology { get; set; }
+        public string? Intencity { get; set; }
         public List<ProductTaste> Tastes { get; set; } = new();
-        public string? TypeCoffee { get; set; }
-        public string? KindCoffee { get; set; }
-        public string? ConsistencyRelease { get; set; }
-        public string? Сomposition { get; set; }
-        public string? IntencityRelease { get; set; }
-        public bool IsCoffeinFrree { get; set; }
+        public string? Description { get; set; }
+        public float Protein { get; set; }
+        public float Fat { get; set; }
+        public float Sugar { get; set; }
+        public float Energy { get; set; }
+        public bool IsCoffeinFree { get; set; }
+        public string? Maker { get; set; }
+        public int CreatedDate { get; set; }
 
 
         public void Mapping(Profile profile)
@@ -24,20 +31,18 @@ namespace Items.Domain.ViewModel.EntitiesVm.CoffeeProduct
                     opt => opt.MapFrom(map => map.Id))
                 .ForMember(map => map.BrandName,
                     opt => opt.MapFrom(map => map.Brand!.BrandName))
-                .ForMember(map => map.Price,
-                    opt => opt.MapFrom(map => map.Price))
                 .ForMember(map => map.ImagePath,
                     opt => opt.MapFrom(map => map.ImagePath))
-                .ForMember(map => map.Maker,
-                    opt => opt.MapFrom(map => map.Maker))
-                .ForMember(map => map.CountInPackage,
-                    opt => opt.MapFrom(map => map.CountInPackage))
-                .ForMember(map => map.Weight,
-                    opt => opt.MapFrom(map => map.Weight))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price))
+                .ForMember(map => map.Technology,
+                    opt => opt.MapFrom(map => map.Technology))
+                .ForMember(map => map.Intencity,
+                    opt => opt.MapFrom(map => map.Intencity))
+                .ForMember(map => map.Tastes,
+                    opt => opt.MapFrom(map => map.Tastes))
                 .ForMember(map => map.Description,
                     opt => opt.MapFrom(map => map.Description))
-                .ForMember(map => map.IsFarmer,
-                    opt => opt.MapFrom(map => map.IsFarmer))
                 .ForMember(map => map.Protein,
                     opt => opt.MapFrom(map => map.Protein))
                 .ForMember(map => map.Fat,
@@ -46,26 +51,12 @@ namespace Items.Domain.ViewModel.EntitiesVm.CoffeeProduct
                     opt => opt.MapFrom(map => map.Sugar))
                 .ForMember(map => map.Energy,
                     opt => opt.MapFrom(map => map.Energy))
+                .ForMember(map => map.IsCoffeinFree,
+                    opt => opt.MapFrom(map => map.IsCoffeinFree))
+                .ForMember(map => map.Maker,
+                    opt => opt.MapFrom(map => map.Maker))
                 .ForMember(map => map.CreatedDate,
-                    opt => opt.MapFrom(map => map.CreatedDate))
-                .ForMember(map => map.MinTemp,
-                    opt => opt.MapFrom(map => map.MinTemp))
-                .ForMember(map => map.MaxTemp,
-                    opt => opt.MapFrom(map => map.MaxTemp))
-                .ForMember(map => map.Tastes,
-                    opt => opt.MapFrom(map => map.Tastes))
-                .ForMember(map => map.TypeCoffee,
-                    opt => opt.MapFrom(map => map.TypeCoffee))
-                .ForMember(map => map.KindCoffee,
-                    opt => opt.MapFrom(map => map.KindCoffee))
-                .ForMember(map => map.ConsistencyRelease,
-                    opt => opt.MapFrom(map => map.ConsistencyRelease))
-                .ForMember(map => map.Сomposition,
-                    opt => opt.MapFrom(map => map.Composition))
-                .ForMember(map => map.IntencityRelease,
-                    opt => opt.MapFrom(map => map.IntencityRelease))
-                .ForMember(map => map.IsCoffeinFrree,
-                    opt => opt.MapFrom(map => map.IsCoffeinFree));
+                    opt => opt.MapFrom(map => map.CreatedDate));
         }
     }
 }
