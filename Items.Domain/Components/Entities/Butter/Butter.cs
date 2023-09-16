56d2c1a75cb93bb8f5e617438e70692e72801274
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-16 23:54:24 +0300

    Обновил класс Butter

--- Items.Domain/Components/Entities/Butter/Butter.cs
+++ Items.Domain/Components/Entities/Butter/Butter.cs
@@ -4,8 +4,6 @@ namespace Items.Domain.Components.Entities.Butter
 {
     public class Butter : ItemFood
     {
-        public List<ProductTaste> Tastes { get; private set; } = new();
-        public List<FatContentProduct> FatContents { get; private set; } = new();
-        public bool IsVegan { get; private set; }
+        public float FatContent { get; private set; }
     }
 }
