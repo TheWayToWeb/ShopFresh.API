3dd073f4c8c7ed0ed6a31d640eb40266475d13c4
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-16 23:58:24 +0300

    Обновление модели ItemFood

--- Items.Domain/Components/Entities/Common/ItemFood.cs
+++ Items.Domain/Components/Entities/Common/ItemFood.cs
@@ -8,8 +8,7 @@
         public float Sugar { get; private set; }
         public float Energy { get; private set; }
         public int CreatedDate { get; private set; }
-        public int MinTemp { get; private set; }
-        public int MaxTemp { get; private set; }
-
+        public float MinTemp { get; private set; }
+        public float MaxTemp { get; private set; }
     }
 }
