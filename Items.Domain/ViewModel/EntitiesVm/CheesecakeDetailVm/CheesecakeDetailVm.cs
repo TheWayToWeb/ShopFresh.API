f944e449fbbdb187919090084838c5d9a548bdcb
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-20 01:12:04 +0300

    Добавлена модель представления для каталога с чизкейками

--- Items.Domain/ViewModel/EntitiesVm/CheesecakeDetailVm/CheesecakeDetailVm.cs
+++ Items.Domain/ViewModel/EntitiesVm/CheesecakeDetailVm/CheesecakeDetailVm.cs
@@ -1,4 +1,5 @@
-﻿using Items.Domain.Components.Entities.Cheecakes;
+﻿using AutoMapper;
+using Items.Domain.Components.Entities.Cheecakes;
 using Items.Domain.DTOs.Common;
 
 namespace Items.Domain.ViewModel.EntitiesVm.CheesecakeDetailVm
@@ -20,6 +21,37 @@ namespace Items.Domain.ViewModel.EntitiesVm.CheesecakeDetailVm
         public int CreatedDate { get; set; }
         public string? Description { get; set; }
 
-
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Cheesecake, CheesecakeDetailVm>()
+                .ForMember(map => map.Id,
+                    opt => opt.MapFrom(map => map.Id))
+                .ForMember(map => map.BrandName,
+                    opt => opt.MapFrom(map => map.Brand!.BrandName))
+                .ForMember(map => map.ImagePath,
+                    opt => opt.MapFrom(map => map.ImagePath))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price))
+                .ForMember(map => map.CoolingMode,
+                    opt => opt.MapFrom(map => map.CoolingMode))
+                .ForMember(map => map.Protein,
+                    opt => opt.MapFrom(map => map.Protein))
+                .ForMember(map => map.Fat,
+                    opt => opt.MapFrom(map => map.Fat))
+                .ForMember(map => map.Sugar,
+                    opt => opt.MapFrom(map => map.Sugar))
+                .ForMember(map => map.Energy,
+                    opt => opt.MapFrom(map => map.Energy))
+                .ForMember(map => map.Weight,
+                    opt => opt.MapFrom(map => map.Weight))
+                .ForMember(map => map.MinTemp,
+                    opt => opt.MapFrom(map => map.MinTemp))
+                .ForMember(map => map.MaxTemp,
+                    opt => opt.MapFrom(map => map.MaxTemp))
+                .ForMember(map => map.CreatedDate,
+                    opt => opt.MapFrom(map => map.CreatedDate))
+                .ForMember(map => map.Description,
+                    opt => opt.MapFrom(map => map.Description));
+        }
     }
 }
