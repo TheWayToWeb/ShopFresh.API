bf2da6e7365c4d3931d0804dd258c955780fc20e
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-20 01:14:34 +0300

    Удалено поле вкус

--- Items.Domain/Components/Entities/Milk/Milk.cs
+++ Items.Domain/Components/Entities/Milk/Milk.cs
@@ -4,9 +4,8 @@ namespace Items.Domain.Components.Entities.Milk
 {
     public class Milk : ItemFood
     {
-        public List<FlavorName> Tastes { get; private set; } = new();
         public List<FatContentProduct> FatContents { get; private set; } = new();
         public float Volume { get; set; }
-        public bool IsMilkSugarFree { get; private set; }
+        public bool IsLactoFree { get; private set; }
     }
 }
