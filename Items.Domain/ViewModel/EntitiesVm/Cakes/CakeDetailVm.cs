2b061b1489804aeb349bac206dd6f1a8ca98d9a5
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-19 08:57:18 +0300

    Добавлен класс View Model Cake Detail Vm для детальной траницы тортов

---
+++ Items.Domain/ViewModel/EntitiesVm/Cakes/CakeDetailVm.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Items.Domain.Components.Entities.Cakes;
+using Items.Domain.DTOs.Common;
+
+namespace Items.Domain.ViewModel.EntitiesVm.Cakes
+{
+    public class CakeDetailVm : IMapWith<Cake>
+    {
+        public Guid Id { get; set; }
+        public string? BrandName { get; set; }
+        public string? ImagePath { get; set; }
+        public int Price { get; set; }
+        public string? CakeType { get; set; }
+        public float Energy { get; set; }
+        public float Protein { get; set; }
+        public float Fat { get; set; }
+        public float Sugar { get; set; }
+        public string? Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Cake, CakeDetailVm>()
+                .ForMember(map => map.Id,
+                    opt => opt.MapFrom(map => map.Id))
+                .ForMember(map => map.BrandName,
+                    opt => opt.MapFrom(map => map.Brand!.BrandName))
+                .ForMember(map => map.ImagePath,
+                    opt => opt.MapFrom(map => map.ImagePath))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price))
+                .ForMember(map => map.CakeType,
+                    opt => opt.MapFrom(map => map.CakeType))
+                .ForMember(map => map.Energy,
+                    opt => opt.MapFrom(map => map.Energy))
+                .ForMember(map => map.Protein,
+                    opt => opt.MapFrom(map => map.Protein))
+                .ForMember(map => map.Fat,
+                    opt => opt.MapFrom(map => map.Fat))
+                .ForMember(map => map.Sugar,
+                    opt => opt.MapFrom(map => map.Sugar))
+                .ForMember(map => map.Description,
+                    opt => opt.MapFrom(map => map.Description));
+        }
+    }
+}
