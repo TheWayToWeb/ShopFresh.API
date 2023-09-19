44a029c214245bd14c5d74d70eba9fba882ba3e4
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-20 01:37:48 +0300

    Изменен тип поля Volume

--- Items.Domain/Components/Entities/SoyMilk/SoyMilk.cs
+++ Items.Domain/Components/Entities/SoyMilk/SoyMilk.cs
@@ -4,9 +4,9 @@ namespace Items.Domain.Components.Entities.SoyMilk
 {
     public class SoyMilk : ItemFood
     {
-        public List<FlavorName> Taste { get; private set; } = new();
-        public List<SoldCapacity> Volume { get; private set; } = new();
-        public string? KindOfMilk { get; private set; }
-        public bool IsCold { get; private set; }
+        public List<FlavorName> Tastes { get; private set; } = new();
+        public float FatContent { get; private set; }
+        public float Volume { get; set; }
+        public string? SoyMilkKind { get; private set; }
     }
 }
