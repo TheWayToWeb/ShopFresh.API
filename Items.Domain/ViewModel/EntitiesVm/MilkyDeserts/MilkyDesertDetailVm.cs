77abe7fea648ca960aa56dba7463a7dd02544315
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-20 01:13:50 +0300

    Добавлена модель представления для каталога с молочными десертами

---
+++ Items.Domain/ViewModel/EntitiesVm/MilkyDeserts/MilkyDesertDetailVm.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Items.Domain.Components.Entities.Common;
+using Items.Domain.Components.Entities.JuiceDessert;
+using Items.Domain.DTOs.Common;
+
+namespace Items.Domain.ViewModel.EntitiesVm.MilkyDeserts
+{
+    public class MilkyDesertDetailVm : IMapWith<MilkyDesert>
+    {
+        public Guid Id { get; set; }
+        public string? BrandName { get; set; }
+        public string? ImagePath { get; set; }
+        public int Price { get; set; }
+        public List<FlavorName> Tastes { get; set; } = new();
+        public float FatContent { get; set; }
+        public float Weight { get; set; }
+        public float Protein { get; set; }
+        public float Fat { get; set; }
+        public float Sugar { get; set; }
+        public float Energy { get; set; }
+        public string? Description { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<MilkyDesert, MilkyDesertDetailVm>()
+                .ForMember(map => map.Id,
+                    opt => opt.MapFrom(map => map.Id))
+                .ForMember(map => map.BrandName,
+                    opt => opt.MapFrom(map => map.Brand!.BrandName))
+                .ForMember(map => map.ImagePath,
+                    opt => opt.MapFrom(map => map.ImagePath))
+                .ForMember(map => map.Price,
+                    opt => opt.MapFrom(map => map.Price))
+                .ForMember(map => map.Tastes,
+                    opt => opt.MapFrom(map => map.Tastes))
+                .ForMember(map => map.FatContent,
+                    opt => opt.MapFrom(map => map.FatContent))
+                .ForMember(map => map.Weight,
+                    opt => opt.MapFrom(map => map.Weight))
+                .ForMember(map => map.Protein,
+                    opt => opt.MapFrom(map => map.Protein))
+                .ForMember(map => map.Fat,
+                    opt => opt.MapFrom(map => map.Fat))
+                .ForMember(map => map.Sugar,
+                    opt => opt.MapFrom(map => map.Sugar))
+                .ForMember(map => map.Energy,
+                    opt => opt.MapFrom(map => map.Energy))
+                .ForMember(map => map.Description,
+                    opt => opt.MapFrom(map => map.Description));
+        }
+    }
+}
