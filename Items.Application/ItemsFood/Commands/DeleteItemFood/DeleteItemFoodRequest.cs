5e01fc319740ab9124d8f44ec47978f92c7a0c60
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-24 02:09:22 +0300

    Создал команду удаления продукта

---
+++ Items.Application/ItemsFood/Commands/DeleteItemFood/DeleteItemFoodRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Items.Application.ItemsFood.Commands.DeleteItemFood
+{
+    public class DeleteItemFoodRequest : IRequest<Unit>
+    {
+        public Guid ItemId { get; set; }
+    }
+}
---
+++ Items.Application/ItemsFood/Commands/DeleteItemFood/DeleteItemFoodRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using Items.Application.Interfaces;
+using MediatR;
+
+namespace Items.Application.ItemsFood.Commands.DeleteItemFood
+{
+    public class DeleteItemFoodRequestHandler : IRequestHandler<DeleteItemFoodRequest, Unit>
+    {
+        private readonly IItemFoodDbContext _dbContext;
+
+        public DeleteItemFoodRequestHandler(IItemFoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(DeleteItemFoodRequest request, CancellationToken cancellationToken)
+        {
+            var itemFood = await _dbContext.ItemFoods.FindAsync(new object[] { request.ItemId }, cancellationToken) ?? throw new NotImplementedException();
+
+            _dbContext.ItemFoods.Remove(itemFood);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
