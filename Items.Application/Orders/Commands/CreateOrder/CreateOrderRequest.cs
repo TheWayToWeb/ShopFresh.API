2b50fcef7f7e5f69c4841c632107ddc122c68499
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-24 02:49:28 +0300

    Написал команду создания заказа

---
+++ Items.Application/Orders/Commands/CreateOrder/CreateOrderRequest.cs
@@ -0,0 +1,12 @@
+﻿using Items.Domain.Components.Items;
+using MediatR;
+
+namespace Items.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderRequest : IRequest<Guid>
+    {
+        public Guid UserId { get; set; }
+        public Guid OrderId { get; set; }
+        public List<Item>? Items { get; set; }
+    }
+}
---
+++ Items.Application/Orders/Commands/CreateOrder/CreateOrderRequestHandler.cs
@@ -0,0 +1,31 @@
+﻿using Items.Application.Interfaces;
+using Items.Domain.Components.Orders;
+using MediatR;
+
+namespace Items.Application.Orders.Commands.CreateOrder
+{
+    public class CreateOrderRequestHandler : IRequestHandler<CreateOrderRequest, Guid>
+    {
+        private readonly IOrderDbContext _dbContext;
+
+        public CreateOrderRequestHandler(IOrderDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Guid> Handle(CreateOrderRequest request, CancellationToken cancellationToken)
+        {
+            var order = new Booking
+            {
+                UserId = request.UserId,
+                Id = request.OrderId,
+                Items = request.Items
+            };
+
+            await _dbContext.Bookings.AddAsync(order, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return order.Id;
+        }
+    }
+}
