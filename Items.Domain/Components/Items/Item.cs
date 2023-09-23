7cb4624e80f1ec1c85d9df6e0506338e8b1093c5
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-24 02:00:19 +0300

    Переместил классы товаров в директорию Items

---
+++ Items.Domain/Components/Items/Item.cs
@@ -0,0 +1,16 @@
+﻿using Items.Domain.Components.Entities.Common;
+
+namespace Items.Domain.Components.Items
+{
+    public abstract class Item
+    {
+        public Guid Id { get; set; }
+        public Guid BrandId { get; set; }
+        public Brand? Brand { get; set; }
+        public int Price { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Maker { get; set; }
+        public int CountInPackage { get; set; }
+        public float Weight { get; set; }
+        public string? Description { get; set; }
+}
---
+++ Items.Domain/Components/Items/ItemFood.cs
@@ -0,0 +1,14 @@
+﻿namespace Items.Domain.Components.Items
+{
+    public class ItemFood : Item
+    {
+        public bool IsFarmer { get; private set; }
+        public float Protein { get; private set; }
+        public float Fat { get; private set; }
+        public float Sugar { get; private set; }
+        public float Energy { get; private set; }
+        public int CreatedDate { get; private set; }
+        public float MinTemp { get; private set; }
+        public float MaxTemp { get; private set; }
+    }
+}
