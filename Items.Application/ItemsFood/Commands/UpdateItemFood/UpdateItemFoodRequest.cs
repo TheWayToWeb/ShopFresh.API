0e7b8361a1a4b3c037c50eb545ac0741d7bd0fe9
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-24 02:09:41 +0300

    Создал команду обновления продукта

---
+++ Items.Application/ItemsFood/Commands/UpdateItemFood/UpdateItemFoodRequest.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Items.Application.ItemsFood.Commands.UpdateItemFood
+{
+    public class UpdateItemFoodRequest : IRequest<Unit>
+    {
+        public Guid ItemId { get; set; }
+        public Guid BrandId { get; set; }
+        public int Price { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Maker { get; set; }
+        public int CountInPackage { get; set; }
+        public float Weight { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Items.Application/ItemsFood/Commands/UpdateItemFood/UpdateItemFoodRequestHandler.cs
@@ -0,0 +1,33 @@
+﻿using Items.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Items.Application.ItemsFood.Commands.UpdateItemFood
+{
+    public class UpdateItemFoodRequestHandler : IRequestHandler<UpdateItemFoodRequest, Unit>
+    {
+        private readonly IItemFoodDbContext _dbContext;
+
+        public UpdateItemFoodRequestHandler(IItemFoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(UpdateItemFoodRequest request, CancellationToken cancellationToken)
+        {
+            var itemFood = await _dbContext.ItemFoods.FirstOrDefaultAsync(item => item.Id == request.ItemId, cancellationToken) ?? throw new NotImplementedException();
+
+            itemFood.BrandId = request.BrandId;
+            itemFood.Price = request.Price;
+            itemFood.ImagePath = request.ImagePath;
+            itemFood.Maker = request.Maker;
+            itemFood.CountInPackage = request.CountInPackage;
+            itemFood.Weight = request.Weight;
+            itemFood.Description = request.Description;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
