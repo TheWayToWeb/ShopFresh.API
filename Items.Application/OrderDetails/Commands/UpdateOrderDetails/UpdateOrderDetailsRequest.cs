afbe7a8aedc64b7f3018b9f99c1a4d22ff318ad3
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-24 02:11:02 +0300

    Создал команду обновления деталей заказа

---
+++ Items.Application/OrderDetails/Commands/UpdateOrderDetails/UpdateOrderDetailsRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Items.Application.OrderDetails.Commands.UpdateOrderDetails
+{
+    public class UpdateOrderDetailsRequest : IRequest<Unit>
+    {
+        public Guid OrderId { get; set; }
+        public float OrderPrice { get; set; }
+        public DateTime DispatchDate { get; set; }
+        public DateTime ReceivingDate { get; set; }
+    }
+}
---
+++ Items.Application/OrderDetails/Commands/UpdateOrderDetails/UpdateOrderDetailsRequestHandler.cs
@@ -0,0 +1,29 @@
+﻿using Items.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Items.Application.OrderDetails.Commands.UpdateOrderDetails
+{
+    public class UpdateOrderDetailsRequestHandler : IRequestHandler<UpdateOrderDetailsRequest, Unit>
+    {
+        private readonly IBookingrDetailsDbContext _dbContext;
+
+        public UpdateOrderDetailsRequestHandler(IBookingrDetailsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderDetailsRequest request, CancellationToken cancellationToken)
+        {
+            var bookingDetails = await _dbContext.BookingDetails.FirstOrDefaultAsync(item => item.Id == request.OrderId, cancellationToken) ?? throw new NotImplementedException();
+
+            bookingDetails.OrderPrice = request.OrderPrice;
+            bookingDetails.DispatchDate = request.DispatchDate;
+            bookingDetails.ReceivingDate = request.ReceivingDate;
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
