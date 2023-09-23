a108c51674faf91d5a8ea150365ccbb43ac62004
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-24 02:10:06 +0300

    Создал команду создания сущности деталей заказа

---
+++ Items.Application/OrderDetails/Commands/CreateOrderDetails/CreateOrderDetailsRequest.cs
@@ -0,0 +1,15 @@
+﻿using Items.Domain.Components.Items;
+using Items.Domain.Components.Orders.Common;
+using Items.Domain.Components.Orders;
+using MediatR;
+
+namespace Items.Application.OrderDetails.Commands.CreateOrderDetails
+{
+    public class CreateOrderDetailsRequest : IRequest<Guid>
+    {
+        public Guid OderId { get; set; }
+        public float OrderPrice { get; set; }
+        public DateTime DispatchDate { get; set; }
+        public DateTime ReceivingDate { get; set; }
+    }
+}
---
+++ Items.Application/OrderDetails/Commands/CreateOrderDetails/CreateOrderDetailsRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using Items.Application.Interfaces;
+using Items.Domain.Components.Orders;
+using MediatR;
+
+namespace Items.Application.OrderDetails.Commands.CreateOrderDetails
+{
+    public class CreateOrderDetailsRequestHandler : IRequestHandler<CreateOrderDetailsRequest, Guid>
+    {
+        private readonly IBookingrDetailsDbContext _dbContext;
+
+        public CreateOrderDetailsRequestHandler(IBookingrDetailsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Guid> Handle(CreateOrderDetailsRequest request, CancellationToken cancellationToken)
+        {
+            var orderDetails = new BookingDetails
+            {
+                OrderId = request.OderId,
+                OrderPrice = request.OrderPrice,
+                DispatchDate = request.DispatchDate,
+                ReceivingDate = request.ReceivingDate
+            };
+
+            await _dbContext.BookingDetails.AddAsync(orderDetails, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return orderDetails.OrderId;
+        }
+    }
+}
