5d1d75a3ce46c9cf45265306f435aa10145757a1
DenisSuperStar <DabBr1994@yandex.ru>
2023-09-24 02:09:05 +0300

    Создал команду создания продукта

---
+++ Items.Application/ItemsFood/Commands/CreateItemFood/CreateItemFoodRequest.cs
@@ -0,0 +1,17 @@
+﻿using Items.Domain.Components.Entities.Common;
+using MediatR;
+
+namespace Items.Application.Items.Commands.CreateItem
+{
+    public class CreateItemFoodRequest : IRequest<Guid>
+    {
+        public Guid ItemId { get; set; }
+        public Guid BrandId { get; set; }
+        public int Price { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Maker { get; set; }
+        public int CountInPackage { get; set; }
+        public float Weight { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ Items.Application/ItemsFood/Commands/CreateItemFood/CreateItemFoodRequestHandler.cs
@@ -0,0 +1,36 @@
+﻿using Items.Application.Interfaces;
+using Items.Domain.Components.Items;
+using MediatR;
+
+namespace Items.Application.Items.Commands.CreateItem
+{
+    public class CreateItemFoodRequestHandler : IRequestHandler<CreateItemFoodRequest, Guid>
+    {
+        private readonly IItemFoodDbContext _dbContext;
+
+        public CreateItemFoodRequestHandler(IItemFoodDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Guid> Handle(CreateItemFoodRequest request, CancellationToken cancellationToken)
+        {
+            var itemFood = new ItemFood
+            {
+                Id = request.ItemId,
+                BrandId = request.BrandId,
+                Price = request.Price,
+                ImagePath = request.ImagePath,
+                Maker = request.Maker,
+                CountInPackage = request.CountInPackage,
+                Weight = request.Weight,
+                Description = request.Description
+            };
+
+            await _dbContext.ItemFoods.AddAsync(itemFood, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return itemFood.Id;
+        }
+    }
+}
