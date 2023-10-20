53a7a76bbedfb3465030c3a3825307592cd3539b
DenisSuperStar <DabBr1994@yandex.ru>
2023-10-20 20:11:23 +0300

    Добавил валидатор для отзывов

--- Items.Domain/Components/Feedback/Review.cs
+++ Items.Domain/Components/Feedback/Review.cs
@@ -5,12 +5,12 @@ namespace Items.Domain.Components.Feedback
 {
     public class Review
     {
-        public Guid Id { get; private set; }
-        public int Rating { get; private set; }
-        public string? Description { get; private set; }
-        public Guid UserId { get; private set; }
-        public User? User { get; set; }
-        public Guid ItemId { get; private set; }
-        public Item? Item { get; set; }
+        public Guid Id { get; set; }
+        public int Rating { get; set; }
+        public string Description { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public Guid ItemId { get; set; }
+        public Item Item { get; set; }
     }
 }
---
+++ Items.Domain/Components/Feedback/ReviewValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Items.Domain.Components.Feedback
+{
+    public class ReviewValidator : AbstractValidator<Review>
+    {
+        public ReviewValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Description).NotNull();
+            RuleFor(x => x.UserId).NotNull();
+            RuleFor(x => x.User).NotNull();
+            RuleFor(x => x.ItemId).NotNull();
+            RuleFor(x => x.Item).NotNull();
+        }
+    }
+}
