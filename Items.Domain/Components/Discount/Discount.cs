51cd00d0349545a1399547bd46596b308cacf8b1
DenisSuperStar <DabBr1994@yandex.ru>
2023-10-20 20:11:00 +0300

    Добавил валидатор DiscountValidator

--- Items.Domain/Components/Discount/Discount.cs
+++ Items.Domain/Components/Discount/Discount.cs
@@ -4,11 +4,11 @@ namespace Items.Domain.Components.Discount
 {
     public class Discount
     {
-        public Guid Id { get; private set; }
-        public float Percent { get; private set; }
-        public DateTime StartDiscount { get; private set; }
-        public DateTime FinishDiscount { get; private set; }
-        public Guid ItemId { get; private set; }
-        public Item? Item { get; private set; }
+        public Guid Id { get; set; }
+        public float Percent { get; set; }
+        public DateTime StartDiscount { get; set; }
+        public DateTime FinishDiscount { get; set; }
+        public Guid ItemId { get; set; }
+        public Item Item { get; set; }
     }
 }
---
+++ Items.Domain/Components/Discount/DiscountValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Items.Domain.Components.Discount
+{
+    public class DiscountValidator : AbstractValidator<Discount>
+    {
+        public DiscountValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.ItemId).NotNull();
+            RuleFor(x => x.Item).NotNull();
+        }
+    }
+}
