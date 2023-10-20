c3bde1e198c3702e233c248958ae9766d357d1b5
DenisSuperStar <DabBr1994@yandex.ru>
2023-10-20 20:11:44 +0300

    Добавил валидатор для товаров

--- Items.Domain/Components/Items/Item.cs
+++ Items.Domain/Components/Items/Item.cs
@@ -6,11 +6,11 @@ namespace Items.Domain.Components.Items
     {
         public Guid Id { get; set; }
         public Guid BrandId { get; set; }
-        public Brand? Brand { get; set; }
+        public Brand Brand { get; set; }
         public int Price { get; set; }
-        public string? ImagePath { get; set; }
-        public string? Maker { get; set; }
+        public string ImagePath { get; set; }
+        public string Maker { get; set; }
         public int CountInPackage { get; set; }
         public float Weight { get; set; }
-        public string? Description { get; set; }
+        public string Description { get; set; }
 }
---
+++ Items.Domain/Components/Items/ItemFoodValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace Items.Domain.Components.Items
+{
+    public class ItemFoodValidator : AbstractValidator<ItemFood>
+    {
+        public ItemFoodValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Brand).NotNull();
+            RuleFor(x => x.ImagePath).NotNull();
+            RuleFor(x => x.Maker).NotNull();
+            RuleFor(x => x.Maker).Length(0, 25);
+            RuleFor(x => x.Description).NotNull();
+        }
+    }
+}
